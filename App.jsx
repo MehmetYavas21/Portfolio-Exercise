aba091a63648d7826046f1fcab3b89404a98311f
Mehmet Yavas <107928102+MehmetYavas21@users.noreply.github.com>
2023-05-20 12:55:36 +0200

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { PortfolioItemPage } from './pages/PortfolioItemPage';
+import { PortfolioPage } from './pages/PortfolioPage';
+import { portfolioItems } from './utils/data';
+
+export const App = () => {
+  console.log(portfolioItems);
+  const [selectedItem, setSelectedItem] = useState(portfolioItems[0]);
+
+  return (
+    <div className="App">
+      {selectedItem ? (
+        <PortfolioItemPage item={selectedItem} />
+      ) : (
+        <PortfolioPage />
+      )}
+    </div>
+  );
+};
