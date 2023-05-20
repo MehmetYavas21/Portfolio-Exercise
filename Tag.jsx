1a79abc2d2295436a60b61392a7161bb4399a790
Mehmet Yavas <107928102+MehmetYavas21@users.noreply.github.com>
2023-05-20 14:33:36 +0200

    Create Tag.jsx but later change name to PortfolioItemPage.jsx
    Don't forget to create a folder called components and put this file as PortfolioItemPage.jsx in addition to be able to call it from other PortfolioItemPage.jsx component!

---
+++ Tag.jsx
@@ -0,0 +1,18 @@
+import { Tag as CTag } from '@chakra-ui/react';
+
+export const Tag = ({ children, ...props }) => {
+  return (
+    <CTag
+      p={2}
+      size="sm"
+      variant="solid"
+      bgColor="blue.300"
+      color="white"
+      {...props}
+    >
+      {children}
+    </CTag>
+  );
+};
+
+// this componant's name should be PortfolioItemPage.jsx
